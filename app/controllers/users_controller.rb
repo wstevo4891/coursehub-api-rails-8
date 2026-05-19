19dd14813fe6874eee0ee2a25f67496771f30991
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-19 11:15:43 -0700

    add resourceful methods in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :update, :destroy ]
+
   def index
-    render json: { users: User.all }
+    @users = User.all
+    render json: @users, status: :ok
   end
 
   def show
-    user = User.find_by(id: params[:id])
-
-    if user
-      render json: user, status: :ok
-    else
-      render json: { error: "User not found" }, status: :not_found
-    end
+    render json: @user, status: :ok
   end
 
   def create
@@ -23,7 +20,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: { errors: @user.errors.to_hash }, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
