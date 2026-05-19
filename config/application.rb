a8de0cafc0588142d12e52205347c4aa0bde06d8
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-19 14:41:13 -0700

    set rails generators to use active record

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,9 @@ module CoursehubApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.orm :active_record
+    end
   end
 end
