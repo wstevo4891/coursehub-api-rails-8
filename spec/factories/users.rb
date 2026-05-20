db9a2afd3d6d46ab918b8af3d038b8286619a38c
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-20 10:34:31 -0700

    add users factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name { "Test User" }
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password { "password123" }
+    password_confirmation { "password123" }
+    is_admin { false }
+  end
+end
