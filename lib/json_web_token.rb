74414789f43f4288aa77e8e7a895855a8f8cc7fa
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-20 12:50:00 -0700

    reduce default expiration date in JsonWebToken to 15 minutes

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,7 +1,7 @@
 class JsonWebToken
   SECRET_KEY = Rails.application.credentials.secret_key_base
 
-  def self.encode(payload, exp = 24.hours.from_now)
+  def self.encode(payload, exp = 15.minutes.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY)
   end
