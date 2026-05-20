35c5224cdf079c17fab2dc9326deeb9610afb5fd
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-20 15:12:51 -0700

    skip authorization in HealthController

--- app/controllers/api/v1/health_controller.rb
+++ app/controllers/api/v1/health_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class HealthController < ApplicationController
+      skip_before_action :authorize_request
+
       def index
         render json: { status: "ok" }
       end
