010ae7879400e271481037f83782f322cd5c8fca
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-20 11:42:30 -0700

    add password_confirmation field to strict user_params method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -44,7 +44,12 @@ module Api
       end
 
       def user_params
-        params.require(:user).permit(:name, :email, :password)
+        params.require(:user).permit(
+          :name,
+          :email,
+          :password,
+          :password_confirmation
+        )
       end
     end
   end
