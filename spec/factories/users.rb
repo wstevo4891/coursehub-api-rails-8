b47e26d95ef977e34e18d0ae70c9bfcb6736793a
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-20 17:33:40 -0700

    update users factory, add admin trait

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
   factory :user do
-    name { "Test User" }
-    sequence(:email) { |n| "user#{n}@example.com" }
+    name { Faker::Name.name }
+    sequence(:email) { |n| "testuser#{n}@example.com" }
     password { "password123" }
     password_confirmation { "password123" }
     is_admin { false }
+
+    trait :admin do
+      is_admin { true }
+    end
   end
 end
